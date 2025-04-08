59b7bb1a1446781bdb4feb6fa558861bfb569f20
tonipc <tonipc@norarealfood.com>
2025-04-08 17:21:40 +0200

    traduES posnewreprint

---
+++ modules/posnewreprint/translations/es.php
@@ -0,0 +1,6 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{posnewreprint}prestashop>newreprint_8fa1a82de2e891ab7e4b7be147070c40'] = 'Añade tu email para enviarte una copia de este pedido:';
+$_MODULE['<{posnewreprint}prestashop>newreprint_95565c433ac0f5b7fcd148ac58ed51b4'] = 'Envíame un email';
